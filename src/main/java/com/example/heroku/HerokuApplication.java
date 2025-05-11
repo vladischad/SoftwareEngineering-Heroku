9b7bcf724545d366cf71f5e63b57f3a15ea64b91
Vladyslav (Vlad) Maliutin <vladmaliutin@u.boisestate.edu>
2025-05-10 23:30:36 -0600

    getRandomString

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -57,8 +57,8 @@ public class HerokuApplication {
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
+      stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
@@ -85,4 +85,11 @@ public class HerokuApplication {
     }
   }
 
+  public String getRandomString() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 30; i++) {
+      sb.append((char) ((Math.random() * 26) + 'a'));
+    }
+    return sb.toString();
+  } 
 }
